7a6409c87ef832bc20426dc1099c4376050a6c45
Matthew Stroble <Chimiara999@msn.com>
2023-03-13 17:29:09 -0600

    added logging system

--- src/main.py
+++ src/main.py
@@ -3,14 +3,17 @@ import settings
 from discord import app_commands
 from discord.ext import commands
 
-
-
+logger = settings.logging.getLogger("bot")
 
 
 def run():
-    bot = commands.Bot(command_prefix="!", intents= discord.Intents.all())
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix="!", intents=intents )
+
+
     @bot.event
     async def on_ready():
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
         print(f'We have logged in as {bot.user}')
         try:
             synced = await bot.tree.sync()
@@ -23,8 +26,7 @@ def run():
     async def hello(interaction: discord.Interaction): 
         await interaction.response.send_message(f"Hey {interaction.user.mention}! You used a slash command.", ephemeral=True)
 
-
-    bot.run(settings.DISCORD_API_SECRET)
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
 
 if __name__=="__main__":
