b11d4816ca032ed09408fd7d362d95de232ff8b2
Matthew Stroble <Chimiara999@msn.com>
2023-03-13 17:37:10 -0600

    testing

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 
 logger = settings.logging.getLogger("bot")
 
-
 def run():
     intents = discord.Intents.all()
     bot = commands.Bot(command_prefix="!", intents=intents )
@@ -15,16 +14,22 @@ def run():
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
         print(f'We have logged in as {bot.user}')
-        try:
-            synced = await bot.tree.sync()
-            print(f"Synced {len(synced)} command(s)")
-        except Exception as e:
-            print(f"Error syncing commands: {e}")
+        # try:
+        #     synced = await bot.tree.sync()
+        #     print(f"Synced {len(synced)} command(s)")
+        # except Exception as e:
+        #     print(f"Error syncing commands: {e}")
 
+    @bot.command(help="This is Help",
+    description="This is description",
+    brief = "This is brief")
 
-    @bot.tree.command(name="hello")
-    async def hello(interaction: discord.Interaction): 
-        await interaction.response.send_message(f"Hey {interaction.user.mention}! You used a slash command.", ephemeral=True)
+    async def ping(ctx):
+        await ctx.send("Pong")
+
+    # @bot.tree.command(name="hello")
+    # async def hello(interaction: discord.Interaction): 
+    #     await interaction.response.send_message(f"Hey {interaction.user.mention}! You used a slash command.", ephemeral=True)
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
