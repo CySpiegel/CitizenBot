9b02d80528768a8c4fda18c8288b03b9e7313b8f
Matthew Stroble <chimiara999@msn.com>
2023-11-05 04:55:01 +0000

    fixing bot

--- src/main.py
+++ src/main.py
@@ -5,46 +5,35 @@ from discord.ext import commands
 
 logger = settings.logging.getLogger("bot")
 
-def run():
-    intents = discord.Intents.all()
-    bot = commands.Bot(command_prefix="!", intents=intents )
 
+def run():
+    bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
     @bot.event
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
-        print(f'We have logged in as {bot.user}')
-        # try:
-        #     synced = await bot.tree.sync()
-        #     print(f"Synced {len(synced)} command(s)")
-        # except Exception as e:
-        #     print(f"Error syncing commands: {e}")
+        print(f"We have logged in as {bot.user}")
+        try:
+            synced = await bot.tree.sync()
+            print(f"Synced {len(synced)} command(s)")
+        except Exception as e:
+            print(f"Error syncing commands: {e}")
 
-    @bot.command(
-        name="ping", 
-        aliases=['p'],
-        help="This is Help",
-        description="This is description",
-        brief = "This is brief")
+    @bot.tree.command(name="hello")
+    async def hello(interaction: discord.Interaction):
+        await interaction.response.send_message(
+            f"Hey {interaction.user.mention}! You used a slash command.", ephemeral=True
+        )
 
-    async def ping(ctx):
-        '''Answers with pong'''
-        await ctx.send("Pong")
-
-    @bot.command()
-    async def say(ctx, what = "WAT?"):
-        await ctx.send(what)
-
-    @bot.command()
-    async def say2(ctx, *what):
-        await ctx.send(" ".join(what))
-
-    # @bot.tree.command(name="hello")
-    # async def hello(interaction: discord.Interaction): 
-    #     await interaction.response.send_message(f"Hey {interaction.user.mention}! You used a slash command.", ephemeral=True)
+    @bot.tree.command(name="say2")
+    @app_commands.describe(thing_to_say="What should i say?")
+    async def say2(interaction: discord.Interaction, thing_to_say: str):
+        await interaction.response.send_message(
+            f"{interaction.user.name} said: `{thing_to_say}`", ephemeral=True
+        )
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     run()
